9ec3a15f868116b41fbe1b3f5cc75ffa18368224
florin.popa <florin.popa@metrosystems.net>
2021-07-12 20:04:13 +0300

    Add first component

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Popular from './components/Popular';
 import './index.css';
 
 
 class App extends React.Component{
     render(){
         return (
-            <div>
-                Helloooo World!
+            <div className='container'>
+                <Popular/>
             </div>
         )
     }
