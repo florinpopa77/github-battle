47917c048f7d58fdc3ce58b3d92cd32b33b25496
florin.popa <florin.popa@metrosystems.net>
2021-07-12 11:35:14 +0300

    Initial project setup

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                Helloooo World!
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <App/>, 
+    document.getElementById('app')
+)
