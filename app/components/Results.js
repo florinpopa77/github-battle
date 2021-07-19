9614da6b822514bea01fb0a60012801b62506ae3
florin.popa <popa_florinadrian@yahoo.com>
2021-07-19 15:27:38 +0300

    Refactor, add ProfileList component

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { battle } from '../utils/api';
 import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa';
 import Card from './Card';
+import ProfileList from './ProfileList';
 
 export default class Results extends React.Component{
     constructor(props) {
@@ -55,32 +56,7 @@ export default class Results extends React.Component{
                     href={ winner.profile.html_url }
                     name={ winner.profile.login }
                 >
-                    <ul className='card-list'>
-                        <li>
-                            <FaUser color='rgb(239, 115, 115)' size={22} />
-                            { winner.profile.name }
-                        </li>
-                        {winner.profile.location && (
-                            <li>
-                                <FaCompass color='rgb(144, 115, 255)' size={22} />
-                                { winner.profile.location }
-                            </li> 
-                        )}
-                        {winner.profile.company && (
-                            <li>
-                                <FaBriefcase color='#795548' size={22} />
-                                { winner.profile.company }
-                            </li> 
-                        )}
-                        <li>
-                            <FaUsers color='rgb(129, 195, 245)' size={22} />
-                            { winner.profile.followers.toLocaleString() } followers
-                        </li>
-                        <li>
-                            <FaUserFriends color='rgb(64, 183, 95)' size={22} />
-                            { winner.profile.following.toLocaleString() } following
-                        </li>
-                    </ul>
+                    <ProfileList profile={winner.profile}/>
                 </Card>
 
                 <Card
@@ -90,32 +66,7 @@ export default class Results extends React.Component{
                     href={ loser.profile.html_url }
                     name={ loser.profile.login }
                 >
-                    <ul className='card-list'>
-                        <li>
-                            <FaUser color='rgb(239, 115, 115)' size={22} />
-                            { loser.profile.name }
-                        </li>
-                        {loser.profile.location && (
-                            <li>
-                                <FaCompass color='rgb(144, 115, 255)' size={22} />
-                                { loser.profile.location }
-                            </li> 
-                        )}
-                        {loser.profile.company && (
-                            <li>
-                                <FaBriefcase color='#795548' size={22} />
-                                { loser.profile.company }
-                            </li> 
-                        )}
-                        <li>
-                            <FaUsers color='rgb(129, 195, 245)' size={22} />
-                            { loser.profile.followers.toLocaleString() } followers
-                        </li>
-                        <li>
-                            <FaUserFriends color='rgb(64, 183, 95)' size={22} />
-                            { loser.profile.following.toLocaleString() } following
-                        </li>
-                    </ul>
+                    <ProfileList profile={loser.profile}/>
                 </Card>
             </div>
         )
