4759b0884f20dfff6720b23485c674efa2091dd6
florin.popa <popa_florinadrian@yahoo.com>
2021-07-21 14:37:48 +0300

    Add Nav components and implement light/dark mode theme with context

--- app/index.js
+++ app/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
+import Nav from './components/Nav';
+import { ThemeProvider } from './contexts/theme';
 import './index.css';
 
 
 class App extends React.Component{
-    render(){
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({ theme }) => this.setState({ theme: theme === 'light' ? 'dark' : 'light' }))
+            }
+        };
+    }
+
+    render(){``
         return (
-            <div className='container'>
-                <Battle />
-            </div>
+            <ThemeProvider value={ this.state }>
+                <div className={ this.state.theme }>
+                    <div className='container'>
+                        <Nav/>
+                        <Battle />
+                    </div>
+                </div>
+            </ThemeProvider>
         )
     }
 }
